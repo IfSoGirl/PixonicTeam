ad81908c7fa344049733d332d704aaccf8fd52c1
Дарья Матюха <matyukha@pixonic.ru>
2016-02-12 17:23:58 +0300

    Initial commit

---
+++ app/utils/Login.js
@@ -0,0 +1,7 @@
+var scopes = 'https://www.googleapis.com/auth/calendar';
+
+
+//For WEB
+
+var clientId = '433633888093-3e1lolt86gbs88nks6cfmp20l22gq95b.apps.googleusercontent.com';
+var apiKey = 'AIzaSyA7jTupE0WjhL4yqvQEsy3wHZ9ONK8bgcw';
